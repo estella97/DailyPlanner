c7a21c2df057394cc53ec3577dbdf24a25948109
Pierre Boerkoel <pierreboerkoel@hotmail.com>
2019-06-07 21:29:50 -0700

    Add basic feelingButtons

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Todo from './components/time'
+import Feelings from './components/feelings/buttons'
 
 function App() {
   return (
     <div>
+      <Feelings/>
       <Todo />
     </div>
   );
--- client/src/components/feelings/buttons.js
+++ client/src/components/feelings/buttons.js
@@ -2,6 +2,22 @@ import React from 'react';
 
 class FeelingButtons extends React.Component {
     render() {
-        return
+        const feelings = [Happy, Sad, Hungry, Active, Lazy, Excited, Friendly, Quiet];
+        // might need to divide to separate components when we add onClick functionality
+        const feelingButtons = feelings.map((feeling) =>
+            <button id={feeling + 'Button'}>{feeling}</button>
+        );
+        return(
+            <div id="buttons">
+                <div className="row">
+                    {feelingButtons.slice(0, 4)}
+                </div>
+                <div className="row">
+                    {feelingButtons.slice(4, 7)}
+                </div>
+            </div>
+        )
     }
 }
+
+export default FeelingButtons
