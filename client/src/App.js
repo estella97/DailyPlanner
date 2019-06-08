bb958ae90c922bb90487e32ea50aa84591213564
Pierre Boerkoel <pierreboerkoel@hotmail.com>
2019-06-07 22:21:49 -0700

    adding a background image and minimal styling for title

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import Todo from './components/time'
 import Feelings from './components/feelings/buttons'
+import './App.css';
 
 function App() {
   return (
     <div>
+      <div className="title">
+        <h1>Plan Me</h1>
+      </div>
       <Feelings/>
       <Todo />
     </div>
