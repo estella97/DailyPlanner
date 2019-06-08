afa411a31ee15170695c290df745b70e6c25af5d
WillCZhang <will.chi.zhang@gmail.com>
2019-06-08 00:26:44 -0700

    added commute

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css'
 import Time from './components/time'
 import Feelings from './components/feelings/buttons'
+import Commute from './components/commute/commute';
 
 function App() {
   return (
     <div className="center">
       <Time />
-      <Feelings/>
+      <Feelings />
+      <Commute />
     </div>
   );
 }
---
+++ client/src/components/commute/commute.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { connect } from "react-redux";
+import './commute.css';
+
+class CommuteButton extends React.Component {
+    render() {
+      const commute = this.props.commute;
+      return (
+        <input type="image" src={process.env.PUBLIC_URL+"/image/"+commute+".png" }
+            onClick={() => this.commuteState = commute} className="horizontal" alt={commute} />
+      );
+    }
+  }
+
+class Commute extends React.Component {
+    render() {
+        const commutes = ["bus", "car", "walk", "bike"];
+        let buttons = commutes.map((commute) => {
+            return <CommuteButton key={commute} commute={commute} />
+        });
+        return (
+            <div>
+                {buttons}
+            </div>
+        )
+    }
+}
+
+const mapStateToProps = (state) => {return {commuteState: state.commute}};
+export default connect(mapStateToProps)(Commute);
