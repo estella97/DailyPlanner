5b3d94ed0ddf4e30a842fceea09e67637aad0019
PierreBoerkoel <pierreboerkoel@hotmail.com>
2019-06-20 16:00:10 -0700

    Connect to mongo

---
+++ backend/server.js
@@ -0,0 +1,70 @@
+const mongoose = require('mongoose');
+const express = require('express');
+var cors = require('cors');
+const bodyParser = require('body-parser');
+const logger = require('morgan');
+const Data = require('./data');
+
+const API_PORT = 3001;
+const app = express();
+app.use(cors());
+const router = express.Router();
+
+const dbRoute =
+  'mongodb+srv://admin:admin@cluster0-an3ch.mongodb.net/test?retryWrites=true&w=majority';
+
+mongoose.connect(dbRoute, { useNewUrlParser: true });
+let db = mongoose.connection;
+db.once('open', () => console.log('connected to the database'));
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(logger('dev'));
+
+router.get('/getData', (req, res) => {
+  Data.find((err, data) => {
+    if (err) return res.json({ success: false, error: err });
+    return res.json({ success: true, data: data });
+  });
+});
+
+router.post('/updateData', (req, res) => {
+  const { id, update } = req.body;
+  Data.findByIdAndUpdate(id, update, (err) => {
+    if (err) return res.json({ success: false, error: err });
+    return res.json({ success: true });
+  });
+});
+
+router.delete('/deleteData', (req, res) => {
+  const { id } = req.body;
+  Data.findByIdAndRemove(id, (err) => {
+    if (err) return res.send(err);
+    return res.json({ success: true });
+  });
+});
+
+router.post('/putData', (req, res) => {
+    let data = new Data();
+
+    const { name, type, feelings, location } = req.body;
+    if (!name || !type || !feelings || !location) {
+        return res.json({
+        success: false,
+        error: 'INVALID INPUTS',
+        });
+    }
+    data.name = name;
+    data.type = type;
+    data.feelings = feelings;
+    data.location = location;
+    data.save((err) => {
+        if (err) return res.json({ success: false, error: err });
+        return res.json({ success: true });
+    });
+});
+
+app.use('/api', router);
+
+app.listen(API_PORT, () => console.log(`LISTENING ON PORT ${API_PORT}`));
