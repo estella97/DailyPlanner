f35e4ce5d79e8d790da04fe46ad9c99a1eb11e48
es97 <estellawang0218@gmail.com>
2019-08-09 22:18:42 -0700

    Added result styling

--- client/src/pages/result/result.js
+++ client/src/pages/result/result.js
@@ -1,19 +1,50 @@
 import React from "react";
+import 'antd/dist/antd.css';
+import "./result.css";
+import { Card, Icon } from 'antd';
 
 class Result extends React.Component {
+    renderCommute = commute => {
+        return (
+            <div>
+                <Icon type="line" rotate="90" style={{ fontSize: '46px', color: '#08c' }}></Icon>
+                <br></br>
+                <br></br>
+                <Card size="small">
+                    <p><h4>Commute Time: {commute.commuteTime}</h4></p>
+                    <p><h4>Commute Type: {commute.commuteType}</h4></p>
+                </Card>
+                <br></br>
+            </div>
+        )
+    }
+
+    renderLocation = place => {
+        return (
+            <div>
+                <Icon type="arrow-down" style={{ fontSize: '46px', color: '#08c' }} theme="outlined"></Icon>
+                <Card size="default" hoverable="true">
+                    <p><h4>Name: {place.name}</h4></p>
+                    <p><h4>Rating: {place.rating}</h4></p>
+                </Card>
+                <br></br>
+            </div>
+        )
+    }
+
     render() {
         let fetchedResults = JSON.parse(localStorage.getItem("results"));
         console.log(fetchedResults);
-        let results = fetchedResults.map(result => {
-            return (
-                <div>
-                    <h4>{result.name}</h4>
-                    <h4>{result.rating}</h4>
-                </div>
-            )
+        let results = fetchedResults.map((result, index) => {
+            if (index % 2 === 0) {
+                return this.renderCommute(result);
+            } else {
+                return this.renderLocation(result);
+            }
+
         })
         return (
-            <div>
+            <div className="center">
                 {results}
             </div>
         )
